7fee5bbadbb18e03458a690b799c58d2b93d0189
annandiaye11 <anna.ndiaye1@ism.edu.sn>
2025-07-25 17:07:13 +0000

    ss

--- user-service/src/main/java/com/cgl/userservice/web/controllers/AuthController.java
+++ user-service/src/main/java/com/cgl/userservice/web/controllers/AuthController.java
@@ -1,7 +1,12 @@
 package com.cgl.userservice.web.controllers;
 
+import com.cgl.userservice.data.entities.User;
+import com.cgl.userservice.web.dto.RequestDto;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,4 +14,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 public class AuthController {
+
+
 }
